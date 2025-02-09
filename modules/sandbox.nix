2dac00957c72bc0cc14f4d407f6e0df65b8cc2c5
Your Name <you@example.com>
2025-02-09 21:18:28 +0100

    Save local changes to sandbox.nix

--- modules/sandbox.nix
+++ modules/sandbox.nix
@@ -7,7 +7,7 @@ with lib; let
   cfg = config.sandbox;
 in {
   options.sandbox = {
-    enable = mkOptionEnable "Enable Sandbox nixos distribution";
+    enable = mkEnableOption "Enable Sandbox nixos distribution";
 
     image = mkOption {
       type = types.str;
@@ -19,11 +19,32 @@ in {
     };
   };
   config = mkIf cfg.enable {
-    virtualisation.oci-containers = {
-      backend = "podman";
-      containers.sandbox = {
-        image = cfg.image;
-        ports = ["${toString cfg.port}:22"];
+    networking.firewall.allowedTCPPorts = [cfg.port];
+    #networking.firewall.extraAllowedTCPPorts = [ cfg.port ];
+    networking.nat.enable = true;
+    containers = {
+      sandbox = {
+        autoStart = true;
+        hostAddress = "172.20.0.2"; # IP of the container (can be adjusted)
+        forwardPorts = [
+          {
+            containerPort = cfg.port;
+            hostPort = cfg.port;
+          }
+        ];
+
+        bindMounts = {
+          "/nix" = {
+            hostPath = "/var/lib/containers/sandbox/nix";
+            isReadOnly = false;
+          };
+          "/etc/nixos" = {
+            hostPath = "/var/lib/containers/sandbox/config";
+            isReadOnly = false;
+          };
+        };
+
+        config = "/var/lib/containers/sandbox/config/configuration.nix";
       };
     };
   };
