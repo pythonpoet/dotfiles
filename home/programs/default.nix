2f527363a6751ea4299991e50c28fb43941b07c4
David Wild <david_wild@gmx.ch>
2025-02-13 12:13:59 +0100

    kernel patches & and gnome windows fixes

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -1,8 +1,15 @@
 {
   pkgs,
   inputs,
+  lib,
   ...
-}: {
+}: let
+  macosLikeWindowControls = pkgs.fetchzip {
+    url = "https://github.com/xiadnoring/macos-like-window-controls/archive/refs/heads/main.zip";
+    sha256 = ""; # Replace with actual hash
+    stripRoot = false;
+  };
+in {
   imports = [
     ./anyrun
     ./browsers/firefox.nix
@@ -42,15 +49,29 @@
     zotero
     owncloud-client
   ];
+  home.file = {
+    # Symlink the extracted theme to ~/.config
+    ".config/macos-like-window-controls" = {
+      source = "${macosLikeWindowControls}";
+      recursive = true;
+    };
+    # Apply GTK CSS customization properly
+    ".config/gtk-3.0/gtk.css" = {
+      text = ''@import "../macos-like-window-controls/gtk-3.0.css";'';
+    };
 
+    ".config/gtk-4.0/gtk.css" = {
+      text = ''@import "../macos-like-window-controls/gtk-4.0.css";'';
+    };
+  };
   dconf = {
     enable = true;
     settings = {
       "org/gnome/desktop/input-sources" = {
         show-all-sources = true;
         sources = [
-          (mkTuple ["xkb" "ch+de"])
-          (mkTuple ["xkb" "us+altgr-intl"])
+          ["xkb" "ch+de"]
+          ["xkb" "us+altgr-intl"]
         ];
         xkb-options = ["terminate:ctrl_alt_bksp"];
       };
@@ -79,18 +100,18 @@
       "org/gnome/desktop/wm/preferences" = {
         "button-layout" = ":minimize,maximize,close";
       };
-      # Should add macos like colour
-      "gtk-config" = {
-        gtk-3-css = ''@import "../macos-like-window-controls/gtk-3.0.css";'';
-        gtk-4-css = ''@import "../macos-like-window-controls/gtk-4.0.css";'';
-      };
-      "environment.systemPackages" = [
-        (pkgs.fetchzip {
-          url = "https://github.com/xiadnoring/macos-like-window-controls/archive/refs/heads/main.zip";
-          sha256 = ""; # Replace this with the actual hash
-          stripRoot = false;
-        })
-      ];
+      # # Should add macos like colour
+      # "gtk-config" = {
+      #   gtk-3-css = ''@import "../macos-like-window-controls/gtk-3.0.css";'';
+      #   gtk-4-css = ''@import "../macos-like-window-controls/gtk-4.0.css";'';
+      # };
+      # "environment.systemPackages" = [
+      #   (pkgs.fetchzip {
+      #     url = "https://github.com/xiadnoring/macos-like-window-controls/archive/refs/heads/main.zip";
+      #     sha256 = ""; # Replace this with the actual hash
+      #     stripRoot = false;
+      #   })
+      # ];
       "org/gnome/desktop/interface" = {
         color-scheme = "prefer-dark";
       };
