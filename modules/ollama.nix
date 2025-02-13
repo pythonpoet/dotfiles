405061846f1fd9c126a9ec8d9cda2c8f5f9099d9
David Wild <david_wild@gmx.ch>
2025-02-13 22:22:54 +0100

    fixes

--- modules/ollama.nix
+++ modules/ollama.nix
@@ -40,7 +40,7 @@ in {
       containers.ollama = {
         image = cfg.image;
         ports = [
-          "${toString cfg.port_web_ui}:8080"
+          #"${toString cfg.port_web_ui}:8080"
           #"${toString cfg.port_ollama}:11434"
         ];
         volumes = [
