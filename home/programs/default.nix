71ad969644e5efb689a357dd028b28dcf5604e39
David Wild <david_wild@gmx.ch>
2025-02-13 11:49:43 +0100

    add kernel Patch and gnome customisation

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -46,6 +46,15 @@
   dconf = {
     enable = true;
     settings = {
+      "org/gnome/desktop/input-sources" = {
+        show-all-sources = true;
+        sources = [
+          (mkTuple ["xkb" "ch+de"])
+          (mkTuple ["xkb" "us+altgr-intl"])
+        ];
+        xkb-options = ["terminate:ctrl_alt_bksp"];
+      };
+
       "org/gnome/shell" = {
         disable-user-extensions = false;
         enabled-extensions = with pkgs.gnomeExtensions; [
@@ -70,6 +79,18 @@
       "org/gnome/desktop/wm/preferences" = {
         "button-layout" = ":minimize,maximize,close";
       };
+      # Should add macos like colour
+      "gtk-config" = {
+        gtk-3-css = ''@import "../macos-like-window-controls/gtk-3.0.css";'';
+        gtk-4-css = ''@import "../macos-like-window-controls/gtk-4.0.css";'';
+      };
+      "environment.systemPackages" = [
+        (pkgs.fetchzip {
+          url = "https://github.com/xiadnoring/macos-like-window-controls/archive/refs/heads/main.zip";
+          sha256 = ""; # Replace this with the actual hash
+          stripRoot = false;
+        })
+      ];
       "org/gnome/desktop/interface" = {
         color-scheme = "prefer-dark";
       };
