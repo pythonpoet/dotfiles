b353dc564d3fa5c4683a6bbdfdb95879609c4626
David Wild <david_wild@gmx.ch>
2025-02-13 20:07:44 +0100

    fixes

--- modules/ollama.nix
+++ modules/ollama.nix
@@ -42,7 +42,7 @@ in {
         volumes = [
           "${cfg.db_path}:/app/backend/data"
         ];
-        extra_options = "--network=host";
+        extraOptions = "--network=host";
       };
     };
     networking.firewall.allowedTCPPorts = [cfg.port_web_ui];
