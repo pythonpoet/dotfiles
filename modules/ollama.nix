7bbd0ecc1f7d90a4395161fd0f8d857a60ab637b
David Wild <david_wild@gmx.ch>
2025-02-15 20:11:39 +0100

    ollama version

--- modules/ollama.nix
+++ modules/ollama.nix
@@ -29,45 +29,57 @@ in {
     };
   };
   config = mkIf cfg.enable {
-    services.ollama = {
-      enable = true;
-      acceleration = "cuda";
-      loadModels = cfg.models;
-      package = pkgs.callPackage ./ollama_package.nix {};
-      # package = pkgs.ollama.overrideAttrs (oldAttrs: {
-      #   src = fetchFromGitHub {
-      #       owner = "ollama";
-      #       repo = "ollama";
-      #       tag = "v${version}";
-      #       hash = "sha256-DW7gHNyW1ML8kqgMFsqTxS/30bjNlWmYmeov2/uZn00=";
-      #       fetchSubmodules = true;
-      #     };
+    # services.ollama = {
+    #   enable = true;
+    #   acceleration = "cuda";
+    #   loadModels = cfg.models;
+    #   package = pkgs.callPackage ./ollama_package.nix {};
+    #   # package = pkgs.ollama.overrideAttrs (oldAttrs: {
+    #   #   src = fetchFromGitHub {
+    #   #       owner = "ollama";
+    #   #       repo = "ollama";
+    #   #       tag = "v${version}";
+    #   #       hash = "sha256-DW7gHNyW1ML8kqgMFsqTxS/30bjNlWmYmeov2/uZn00=";
+    #   #       fetchSubmodules = true;
+    #   #     };
 
-      # });
+    #   # });
 
-      #gport = cfg.port_ollama;
-      openFirewall = true;
-    };
+    #   #gport = cfg.port_ollama;
+    #   openFirewall = true;
+    # };
     virtualisation.oci-containers = {
       backend = "podman";
-      containers.web-llm = {
-        image = cfg.image;
-        ports = [
-          #"${toString cfg.port_web_ui}:8080"
-        ];
-        volumes = [
-          "${cfg.db_path}:/app/backend/data"
-        ];
-        environment = {
-          OLLAMA_BASE_URL = "http://127.0.0.1:11434"; # Set environment variable
+      containers = {
+        web-llm = {
+          image = cfg.image;
+          ports = [
+            #"${toString cfg.port_web_ui}:8080"
+          ];
+          volumes = [
+            "${cfg.db_path}:/app/backend/data"
+          ];
+          environment = {
+            OLLAMA_BASE_URL = "http://127.0.0.1:11434"; # Set environment variable
+          };
+          #comment
+          #extraOptions =
+          #[ "--network=host" "--add-host=host.containers.internal:host-gateway" ];
+          extraOptions = [
+            "--network=host"
+            "--add-host=host.containers.internal:host-gateway"
+          ];
         };
-        #comment
-        #extraOptions =
-        #[ "--network=host" "--add-host=host.containers.internal:host-gateway" ];
-        extraOptions = [
-          "--network=host"
-          "--add-host=host.containers.internal:host-gateway"
-        ];
+        ollama = {
+          image = "ollama/ollama";
+          volumes = [
+            "/var/lib/ollama:/root/.ollama"
+          ];
+          ports = [
+            "11434:11434"
+          ];
+          enableNvidia = true;
+        };
       };
     };
     networking.firewall.allowedTCPPorts = [cfg.port_web_ui];
