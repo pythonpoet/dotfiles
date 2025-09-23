6d5200c841bc2bc2c8bfa781fdf725b0c0ca7518
Your Name <you@example.com>
2025-09-23 14:00:32 +0200

    removing python

--- home/environment/python.nix
+++ home/environment/python.nix
@@ -2,23 +2,23 @@
   python_version = "312";
 in {
   home.packages = [
-    (pkgs.${"python${python_version}"}.withPackages (ps: [
-      ps.numpy
-      ps.pandas
-      ps.matplotlib
-      ps.statsmodels
-      ps.scipy
-      ps.jupyter-core
-      ps.notebook
-      ps.scikit-learn
-      ps.deep-translator
-      ps.tqdm
-      ps.nltk
-      ps.seaborn
-      ps.jupyter
-      ps.kneed
-    ]))
-    (pkgs.poetry.override {python3 = pkgs.${"python${python_version}"};})
-    # (pkgs.uv.override {python3 = pkgs.${"python${python_version}"};})
+    # (pkgs.${"python${python_version}"}.withPackages (ps: [
+    #   ps.numpy
+    #   ps.pandas
+    #   ps.matplotlib
+    #   ps.statsmodels
+    #   ps.scipy
+    #   ps.jupyter-core
+    #   ps.notebook
+    #   ps.scikit-learn
+    #   ps.deep-translator
+    #   ps.tqdm
+    #   ps.nltk
+    #   ps.seaborn
+    #   ps.jupyter
+    #   ps.kneed
+    # ]))
+    # (pkgs.poetry.override {python3 = pkgs.${"python${python_version}"};})
+    # # (pkgs.uv.override {python3 = pkgs.${"python${python_version}"};})
   ];
 }
