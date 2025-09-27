186dccf6b49a73a86c45b2ef093659c924fe1616
Your Name <you@example.com>
2025-09-27 10:40:32 +0200

    some fixes

---
+++ modules/ai/open_webui.nix
@@ -0,0 +1,11 @@
+{
+  services.open-webui = {
+    enable = true;
+    port = 3000;
+    environment = {
+      ANONYMIZED_TELEMETRY = "False";
+      DO_NOT_TRACK = "True";
+      SCARF_NO_ANALYTICS = "True";
+    };
+  };
+}
