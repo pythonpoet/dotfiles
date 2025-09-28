54dba498ac987b2220a491317bc5ef68a70be083
Your Name <you@example.com>
2025-09-28 09:42:43 +0200

    changes

--- hosts/default.nix
+++ hosts/default.nix
@@ -3,44 +3,43 @@
   inputs,
   stdenv,
   ...
-}:
-{
-  flake.nixosConfigurations =
-    let
-      # shorten paths
-      inherit (inputs.nixpkgs.lib) nixosSystem;
+}: {
+  flake.nixosConfigurations = let
+    # shorten paths
+    inherit (inputs.nixpkgs.lib) nixosSystem;
 
-      howdy = inputs.nixpkgs-howdy;
+    howdy = inputs.nixpkgs-howdy;
 
-      homeImports = import "${self}/home/profiles";
+    homeImports = import "${self}/home/profiles";
 
-      mod = "${self}/system";
-      # get the basic config to build on top of
-      inherit (import mod) laptop;
-      #inherit (import "${self}/modules/") _cloud;
-      cloud = [
-        "${self}/modules/owncloud.nix"
-          "${self}/modules/vaultwarden.nix"
-          "${self}/modules/borg.nix"
-          "${self}/modules/vikunja.nix"
-          "${self}/modules/incus.nix"
-          #"${self}/modules/exdetail/"
-          "${self}/modules/ml/zonos.nix"
-          "${self}/modules/ml/kokoro.nix"
-          "${self}/modules/immich.nix"
-          "${self}/modules/postgresql.nix"
-          "${self}/modules/owncloud.nix"
-          "${self}/modules/reverse-proxy.nix"
-      ];
+    mod = "${self}/system";
+    # get the basic config to build on top of
+    inherit (import mod) laptop;
+    #inherit (import "${self}/modules/") _cloud;
+    cloud = [
+      "${self}/modules/owncloud.nix"
+      "${self}/modules/vaultwarden.nix"
+      "${self}/modules/borg.nix"
+      "${self}/modules/vikunja.nix"
+      "${self}/modules/incus.nix"
+      #"${self}/modules/exdetail/"
+      "${self}/modules/ml/zonos.nix"
+      "${self}/modules/ml/kokoro.nix"
+      "${self}/modules/immich.nix"
+      "${self}/modules/postgresql.nix"
+      "${self}/modules/owncloud.nix"
+      "${self}/modules/reverse-proxy.nix"
+    ];
 
-      # get these into the module system
-      specialArgs = { inherit inputs self; };
-    in
-    {
-      alpakapro = nixosSystem {
-        system.stateVersion = 25.11;
-        inherit specialArgs;
-        modules = laptop ++ [
+    # get these into the module system
+    specialArgs = {inherit inputs self;};
+  in {
+    alpakapro = nixosSystem {
+      system.stateVersion = 25.11;
+      inherit specialArgs;
+      modules =
+        laptop
+        ++ [
           # Include surface book specific configuration (only commons)
           inputs.nixos-hardware.nixosModules.microsoft-surface-pro-intel
           inputs.catppuccin.nixosModules.catppuccin
@@ -64,7 +63,7 @@
           }
 
           # enable unmerged Howdy
-          { disabledModules = [ "security/pam.nix" ]; }
+          {disabledModules = ["security/pam.nix"];}
           "${howdy}/nixos/modules/security/pam.nix"
           "${howdy}/nixos/modules/services/security/howdy"
           "${howdy}/nixos/modules/services/misc/linux-enable-ir-emitter.nix"
@@ -73,12 +72,14 @@
           inputs.chaotic.nixosModules.default
           inputs.disko.nixosModules.disko
         ];
-      };
+    };
 
-      alpakabook = nixosSystem {
-        system = "x86_64-linux";
-        inherit specialArgs;
-        modules = laptop ++ [
+    alpakabook = nixosSystem {
+      system = "x86_64-linux";
+      inherit specialArgs;
+      modules =
+        laptop
+        ++ [
           # Include surface book specific configuration (only commons)
           inputs.nixos-hardware.nixosModules.microsoft-surface-common
           inputs.catppuccin.nixosModules.catppuccin
@@ -102,7 +103,7 @@
           }
 
           # enable unmerged Howdy
-          { disabledModules = [ "security/pam.nix" ]; }
+          {disabledModules = ["security/pam.nix"];}
           "${howdy}/nixos/modules/security/pam.nix"
           "${howdy}/nixos/modules/services/security/howdy"
           "${howdy}/nixos/modules/services/misc/linux-enable-ir-emitter.nix"
@@ -111,12 +112,14 @@
           inputs.chaotic.nixosModules.default
           inputs.disko.nixosModules.disko
         ];
-      };
+    };
 
-      alpakapi5 = nixosSystem {
-        system = "aarch64-linux";
-        inherit specialArgs;
-        modules = cloud ++ [
+    alpakapi5 = nixosSystem {
+      system = "aarch64-linux";
+      inherit specialArgs;
+      modules =
+        cloud
+        ++ [
           inputs.catppuccin.nixosModules.catppuccin
           ./alpakapi5
           "${mod}/core/users.nix"
@@ -137,65 +140,64 @@
               data_dir = "/data1/databases";
             };
           }
-
         ];
-      };
-      hal = nixosSystem {
-        inherit specialArgs;
-        system.stateVersion = 25.05;
-        modules = [
-          ./hal
-          "${mod}/core/users.nix"
-          "${mod}/nix"
-          "${mod}/programs/zsh.nix"
-          "${mod}/programs/home-manager.nix"
-
-          {
-            home-manager = {
-              users.david.imports = homeImports."minimal";
-              extraSpecialArgs = specialArgs;
-            };
-            borg.enable = true;
-            borg.startAt = "17:15:00";
-
-            vikunja = {
-              enable = true;
-              db_path = "/var/lib/vikunja/db";
-              files_path = "/var/lib/vikunja/files";
-            };
-            vaultwarden = {
-              enable = true;
-              data_dir = "/var/lib/vaultwarden";
-            };
-            cloud = {
-              enable = true;
-              enable_radicale = true;
-              path_radicale = "/var/lib/radicale/";
-              data_dir = "/var/lib/ocis/data";
-              config_file = "/var/lib/ocis/config/ocis.yaml";
-            };
-            exDetail = {
-              enable = true;
-            };
-            zonos.enable = true;
-            llm = {
-              enable = false;
-              db_path = "/var/lib/ollama";
-              models = [
-                "deepseek-r1:1.5b"
-                "deepseek-r1:8b"
-                "deepseek-r1:14b"
-              ];
-            };
-            incus = {
-              enable = true;
-              enable_networking = true;
-            };
-          }
-
-          #inputs.agenix.nixosModules.default
-          inputs.chaotic.nixosModules.default
-        ];
-      };
     };
+    hal = nixosSystem {
+      inherit specialArgs;
+      system.stateVersion = 25.05;
+      modules = [
+        ./hal
+        "${mod}/core/users.nix"
+        "${mod}/nix"
+        "${mod}/programs/zsh.nix"
+        "${mod}/programs/home-manager.nix"
+
+        {
+          home-manager = {
+            users.david.imports = homeImports."minimal";
+            extraSpecialArgs = specialArgs;
+          };
+          borg.enable = true;
+          borg.startAt = "17:15:00";
+
+          vikunja = {
+            enable = true;
+            db_path = "/var/lib/vikunja/db";
+            files_path = "/var/lib/vikunja/files";
+          };
+          vaultwarden = {
+            enable = true;
+            data_dir = "/var/lib/vaultwarden";
+          };
+          cloud = {
+            enable = true;
+            enable_radicale = true;
+            path_radicale = "/var/lib/radicale/";
+            data_dir = "/var/lib/ocis/data";
+            config_file = "/var/lib/ocis/config/ocis.yaml";
+          };
+          exDetail = {
+            enable = true;
+          };
+          zonos.enable = true;
+          llm = {
+            enable = false;
+            db_path = "/var/lib/ollama";
+            models = [
+              "deepseek-r1:1.5b"
+              "deepseek-r1:8b"
+              "deepseek-r1:14b"
+            ];
+          };
+          incus = {
+            enable = true;
+            enable_networking = true;
+          };
+        }
+
+        #inputs.agenix.nixosModules.default
+        inputs.chaotic.nixosModules.default
+      ];
+    };
+  };
 }
