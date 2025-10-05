4748e105fa3519d10d779b65259a6b287c5bce66
Your Name <you@example.com>
2025-10-06 01:01:00 +0200

    inshallah jmalloc fix

--- hosts/bernina/default.nix
+++ hosts/bernina/default.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, inputs, ... }:
 let
-  kernelBundle = pkgs.linuxAndFirmware.v6_6_31; # or latest supported
+  kernelBundle = pkgs.linuxAndFirmware.v6_12_44; # or latest supported
   nix-settings = ({ config, ... }:{
     nix.registry.nixpkgs.to.path = lib.mkForce inputs.nixpkgs.outPath;
   });
