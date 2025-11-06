c721d95c0cea6628f5d6c6f95bf60f6338ba33a0
Your Name <you@example.com>
2025-11-06 17:41:15 +0100

    adding ocis

--- modules/reverse-proxy.nix
+++ modules/reverse-proxy.nix
@@ -132,7 +132,7 @@ in {
         "cloud.davidwild.ch" = {
          inherit (sslSettings) addSSL enableACME;
           locations."/" = {
-            proxyPass = "http://127.0.0.1:9200";
+            proxyPass = "https://127.0.0.1:9200";
             proxyWebsockets = true;
             extraConfig = extraConfig;
           };
