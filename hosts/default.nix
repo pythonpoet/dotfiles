3a5a8c96cec5d64178b6ea017f6ffd0613ac7e8a
Your Name <you@example.com>
2025-11-07 12:10:24 +0100

    adding vikunja

--- hosts/default.nix
+++ hosts/default.nix
@@ -154,7 +154,7 @@
           };
           vikunja = {
             enable = true;
-            db_path = "/data1/vikunja/db";
+            db_path = "/data1/vikunja/db/vikunja.db";
             files_path = "/data1/vikunja/files";
           };
         }
