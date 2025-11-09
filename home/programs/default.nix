9cd8c8e6ad2dd71ff08d4e98de866489db17edd4
Your Name <you@example.com>
2025-11-09 12:25:39 +0100

    adding ente auth

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -24,6 +24,7 @@
   beeper
   bitwarden-desktop
   brave
+  ente-auth
   calibre
   cheese
   dconf-editor
--- home/terminal/programs/git.nix
+++ home/terminal/programs/git.nix
@@ -2,11 +2,13 @@
   config,
   pkgs,
   ...
-}: let
+}:
+let
   cfg = config.programs.git;
   key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOq9Gew1rgfdIyuriJ/Ne0B8FE1s8O/U2ajErVQLUDu9 mihai@io";
-in {
-  home.packages = [pkgs.gh];
+in
+{
+  home.packages = [ pkgs.gh ];
 
   # enable scrolling in git diff
   home.sessionVariables.DELTA_PAGER = "less -R";
@@ -14,95 +16,106 @@ in {
   programs.git = {
     enable = true;
 
-    delta = {
-      enable = true;
-      options.dark = true;
-    };
-
     extraConfig = {
       diff.colorMoved = "default";
       merge.conflictstyle = "diff3";
     };
 
-    aliases = let
-      log = "log --show-notes='*' --abbrev-commit --pretty=format:'%Cred%h %Cgreen(%aD)%Creset -%C(bold red)%d%Creset %s %C(bold blue)<%an>% %Creset' --graph";
-    in {
-      a = "add --patch"; # make it a habit to consciosly add hunks
-      ad = "add";
-
-      b = "branch";
-      ba = "branch -a"; # list remote branches
-      bd = "branch --delete";
-      bdd = "branch -D";
-
-      c = "commit";
-      ca = "commit --amend";
-      cm = "commit --message";
-
-      co = "checkout";
-      cb = "checkout -b";
-      pc = "checkout --patch";
-
-      cl = "clone";
-
-      d = "diff";
-      ds = "diff --staged";
-
-      h = "show";
-      h1 = "show HEAD^";
-      h2 = "show HEAD^^";
-      h3 = "show HEAD^^^";
-      h4 = "show HEAD^^^^";
-      h5 = "show HEAD^^^^^";
-
-      p = "push";
-      pf = "push --force-with-lease";
-
-      pl = "pull";
-
-      l = log;
-      lp = "${log} --patch";
-      la = "${log} --all";
-
-      r = "rebase";
-      ra = "rebase --abort";
-      rc = "rebase --continue";
-      ri = "rebase --interactive";
-
-      rs = "reset";
-      rsh = "reset --hard"; 
-
-      s = "status --short --branch";
-      ss = "status";
-
-      st = "stash";
-      stc = "stash clear";
-      sth = "stash show --patch";
-      stl = "stash list";
-      stp = "stash pop";
-
-      forgor = "commit --amend --no-edit";
-      oops = "checkout --";
-    };
-
-    ignores = ["*~" "*.swp" "*result*" ".direnv" "node_modules"];
+    ignores = [
+      "*~"
+      "*.swp"
+      "*result*"
+      ".direnv"
+      "node_modules"
+    ];
 
     signing = {
       key = "${config.home.homeDirectory}/.ssh/id_ed25519";
       signByDefault = true;
+      format = "ssh";
     };
 
-    extraConfig = {
-      gpg = {
-        format = "ssh";
-        ssh.allowedSignersFile = config.home.homeDirectory + "/" + config.xdg.configFile."git/allowed_signers".target;
+    settings = {
+
+      alias =
+        let
+          log = "log --show-notes='*' --abbrev-commit --pretty=format:'%Cred%h %Cgreen(%aD)%Creset -%C(bold red)%d%Creset %s %C(bold blue)<%an>% %Creset' --graph";
+        in
+        {
+          a = "add --patch"; # make it a habit to consciosly add hunks
+          ad = "add";
+
+          b = "branch";
+          ba = "branch -a"; # list remote branches
+          bd = "branch --delete";
+          bdd = "branch -D";
+
+          c = "commit";
+          ca = "commit --amend";
+          cm = "commit --message";
+
+          co = "checkout";
+          cb = "checkout -b";
+          pc = "checkout --patch";
+
+          cl = "clone";
+
+          d = "diff";
+          ds = "diff --staged";
+
+          h = "show";
+          h1 = "show HEAD^";
+          h2 = "show HEAD^^";
+          h3 = "show HEAD^^^";
+          h4 = "show HEAD^^^^";
+          h5 = "show HEAD^^^^^";
+
+          p = "push";
+          pf = "push --force-with-lease";
+
+          pl = "pull";
+
+          l = log;
+          lp = "${log} --patch";
+          la = "${log} --all";
+
+          r = "rebase";
+          ra = "rebase --abort";
+          rc = "rebase --continue";
+          ri = "rebase --interactive";
+
+          rs = "reset";
+          rsh = "reset --hard";
+
+          s = "status --short --branch";
+          ss = "status";
+
+          st = "stash";
+          stc = "stash clear";
+          sth = "stash show --patch";
+          stl = "stash list";
+          stp = "stash pop";
+
+          forgor = "commit --amend --no-edit";
+          oops = "checkout --";
+        };
+
+      user = {
+        email = "32130052+pythonpoet@users.noreply.github.com";
+        name = "pythonpoet";
       };
+
+      gpg.ssh.allowedSignersFile =
+        config.home.homeDirectory + "/" + config.xdg.configFile."git/allowed_signers".target;
 
       pull.rebase = true;
     };
-    
-    userEmail = "32130052+pythonpoet@users.noreply.github.com";
-    userName = "pythonpoet";
+  };
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options.dark = true;
   };
 
   xdg.configFile."git/allowed_signers".text = ''
