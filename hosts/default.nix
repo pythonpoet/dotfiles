61f521438a7fe31ad259a816cc42c956ba70a956
Your Name <you@example.com>
2025-11-10 11:16:32 +0100

    disable jitsi

--- hosts/default.nix
+++ hosts/default.nix
@@ -158,6 +158,10 @@
             db_path = "/var/lib/vikunja/vikunja.db";
             files_path = "/data1/vikunja/files";
           };
+          jitsi = {
+            enable = true;
+            domain = "jitsi.davidwild.ch";
+          };
         }
       ];
     };
