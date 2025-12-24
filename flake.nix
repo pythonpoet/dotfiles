cc7aa8734249542254c09403e81a8db92df7fdc2
Your Name <you@example.com>
2025-12-24 16:10:09 +0100

    switch home manager channel to 25.05

--- flake.nix
+++ flake.nix
@@ -79,7 +79,7 @@
     helix.url = "github:helix-editor/helix";
 
     hm = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
