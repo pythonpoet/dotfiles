85d68742dbe9e9336a21e806af44a3d3eddb9c0b
Your Name <you@example.com>
2026-01-08 16:19:58 +0100

    changes to borg job

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   alpakapro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAOr7hdJO0P2TBs5GH+XmOi7XoBT6LiAS7Ym6IEgM2H0 david@alpakapro";
   bernina = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKOc1VdjIPZ92jdNqIkFkn1/C8viTw/7Fqr45bYw0RUA david@bernina";
+  bernina_root = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOKRkROOGrzi/GXpInvhN48oCLYEKIqZHZdP9RPqHi5M root@bernina";
 in {
   #"spotify.age".publicKeys = [mihai io];
-  "borg.age".publicKeys = [bernina];
+  "borg.age".publicKeys = [bernina bernina_root];
 }
