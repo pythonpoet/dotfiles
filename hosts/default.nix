5e4df1d1da97a54c65f73a9b88bf17ff0e85c2f7
Your Name <you@example.com>
2026-01-14 13:24:54 +0100

    update ocis 5.0.1 to 7.0.1

--- hosts/default.nix
+++ hosts/default.nix
@@ -168,7 +168,7 @@
             };
           };
           cloud = {
-            enable =true;
+            enable =false;
             data_dir = "/data1/ocis/data";
             config_file = "/data1/ocis/config/";
           };
