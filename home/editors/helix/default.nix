765d42172a2e32fe017ce327859f7b622c7e46ee
Your Name <you@example.com>
2026-01-18 21:06:58 +0100

    remove helix from src

--- home/editors/helix/default.nix
+++ home/editors/helix/default.nix
@@ -8,7 +8,8 @@
 
   programs.helix = {
     enable = true;
-    package = inputs.helix.packages.${pkgs.system}.default;
+    #dont build from latest
+    #package = inputs.helix.packages.${pkgs.system}.default;
     extraPackages = with pkgs; [
       markdown-oxide
       nodePackages.vscode-langservers-extracted
