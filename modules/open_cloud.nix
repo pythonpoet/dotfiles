1c837cf88e986742e1388cec570dc227e6dc94a6
Your Name <you@example.com>
2026-01-20 18:39:48 +0100

    onlyoffice things

--- modules/open_cloud.nix
+++ modules/open_cloud.nix
@@ -70,7 +70,7 @@ in {
     OC_OIDC_ISSUER = "https://auth.davidwild.ch/application/o/opencloud/";
     PROXY_OIDC_ISSUER = "https://auth.davidwild.ch/application/o/opencloud/";
     OC_EXCLUDE_RUN_SERVICES = "idp";
-    #OC_ADD_RUN_SERVICES = "gateway,app-registry";#,collaboration";
+    OC_ADD_RUN_SERVICES = "collaboration";
 #   gateway
 #   app-registry
 #   collaboration
