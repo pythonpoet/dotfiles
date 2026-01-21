ebca99d1279d2fa43b68617da9e19d4fb8fb6d50
Your Name <you@example.com>
2026-01-21 16:59:18 +0100

    opencloud 3.7.0 to 4.1.0

--- modules/open_cloud.nix
+++ modules/open_cloud.nix
@@ -70,7 +70,7 @@ in {
     OC_OIDC_ISSUER = "https://auth.davidwild.ch/application/o/opencloud/";
     PROXY_OIDC_ISSUER = "https://auth.davidwild.ch/application/o/opencloud/";
     OC_EXCLUDE_RUN_SERVICES = "idp";
-    OC_ADD_RUN_SERVICES = "collaboration";
+    #OC_ADD_RUN_SERVICES = "collaboration";
     OC_LOG_LEVEL = "debug";
     PROXY_TLS = "false";
     HTTP_TLS = "false";
