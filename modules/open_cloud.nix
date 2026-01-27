3a81a1f32f0aa76ee72b7c5a2e6ebf1407533778
Your Name <you@example.com>
2026-01-27 21:13:44 +0100

    wopi problems

--- modules/open_cloud.nix
+++ modules/open_cloud.nix
@@ -106,7 +106,7 @@ in {
     #COLLABORA_DOMAIN = onlyoffice_url;
     #
     FRONTEND_APP_HANDLER_VIEW_APP_ADDR = "eu.opencloud.api.collaboration";
-    #COLLABORA_DOMAIN = "office.davidwild.ch";
+    COLLABORA_DOMAIN = "office.davidwild.ch";
     COLLABORATION_APP_NAME = "OnlyOffice";
 		COLLABORATION_APP_PRODUCT = "OnlyOffice";
 		#COLLABORATION_WOPI_SRC =  "https://wopi.davidwild.ch";#"http://${internal_host}:${toString wopi_port}"; #<- Internal Link to the OpenCloud-Service and add 1/2*
