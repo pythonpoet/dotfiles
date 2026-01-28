d9b7c44b6a52269711542a4d6745a9cc3c49afc8
Your Name <you@example.com>
2026-01-28 01:06:56 +0100

    wopi problems -fix?

--- modules/open_cloud.nix
+++ modules/open_cloud.nix
@@ -215,6 +215,10 @@ in {
     # };
 
     # 2. The VirtualHost Fix: Merges SSL and Redirect logic into the OnlyOffice domain
+    listen = [
+    { addr = "0.0.0.0"; port = 443; ssl = true; }
+    { addr = "127.0.0.1"; port = 443; ssl = true; }
+  ];
     virtualHosts."office.davidwild.ch" = {
       #addSSL = true;
       enableACME = true;
