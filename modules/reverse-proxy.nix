47ec786fffae9d5ec7740f089c21930c71ce68fd
Your Name <you@example.com>
2026-02-04 20:16:15 +0100

    change rounting

--- modules/reverse-proxy.nix
+++ modules/reverse-proxy.nix
@@ -85,23 +85,7 @@ in {
             extraConfig = extraConfig;
           };
         };
-        "audiobookshelf.davidwild.ch" = {
-          inherit (sslSettings) addSSL enableACME;
-          locations."/" = {
-            proxyPass = "http://127.0.0.1:${toString config.services.audiobookshelf.port}";
-            proxyWebsockets = true;
-            extraConfig = extraConfig;
-            # Add these back since you disabled recommendedProxySettings
-            # extraConfig = ''
-            #   proxy_set_header Host $host;
-            #   proxy_set_header X-Real-IP $remote_addr;
-            #   proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            #   proxy_set_header X-Forwarded-Proto $scheme;
-            #   proxy_set_header X-Forwarded-Host $host;
-            #   proxy_set_header X-Forwarded-Port $server_port;
-            # '' + extraConfig;
-          };
-        };
+        
         
         "davidwild.ch" = {
          inherit (sslSettings) addSSL enableACME;
