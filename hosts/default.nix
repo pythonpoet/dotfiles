fed5cc79e842fbad9aebbfabc3ff1873670105d7
Your Name <you@example.com>
2026-02-05 10:04:57 +0100

    typo fix

--- hosts/default.nix
+++ hosts/default.nix
@@ -188,7 +188,7 @@
             };
           };
           cloud = {
-            enable =false;
+            enable =true;
             data_dir = "/data1/opencloud/";
             enable_onlyoffice = true;
             #config_file = "/data1/ocis/config/";
