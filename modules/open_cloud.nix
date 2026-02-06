bf9abc8b972b80ca05adc2e2c639ade70f208430
Your Name <you@example.com>
2026-02-06 15:09:28 +0100

    change onlyoffice to doker immage

--- modules/open_cloud.nix
+++ modules/open_cloud.nix
@@ -287,6 +287,8 @@ in {
             JWT_ENABLED = "true";
             JWT_SECRET="whatever";
             NODE_TLS_REJECT_UNAUTHORIZED = "0";
+            WOPI_SKIP_PROOF = "true";
+            USE_UNAUTHORIZED_STORAGE = "true";
             # ONLYOFFICE_HTTPS_HSTS_ENABLED = "true";
             # PROTO = "http";
             # USE_UNAUTHORIZED_STORAGE = "false";
