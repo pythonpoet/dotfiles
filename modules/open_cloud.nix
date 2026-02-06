3ce607311891dfdc481c6bc1ee52ec1cbd95b41a
Your Name <you@example.com>
2026-02-06 16:30:55 +0100

    change onlyoffice to doker immage

--- modules/open_cloud.nix
+++ modules/open_cloud.nix
@@ -250,13 +250,7 @@ in {
     enableACME = true;
     forceSSL = true;
     locations."/" = {
-      proxyPass = "http://${internal_host}:${toString wopi_port}";
-      extraConfig = ''
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-      '';
+      proxyPass = "http://localhost:${toString wopi_port}";
     };
   };
   };
