60aa3b83bad8ad8f5faa9fd8d33e5fe0181926ad
Your Name <you@example.com>
2026-02-06 13:26:09 +0100

    change onlyoffice to doker immage

--- modules/open_cloud.nix
+++ modules/open_cloud.nix
@@ -225,7 +225,7 @@ in {
     more_clear_headers "X-Frame-Options";
   '';
   locations."/" = {
-    proxyPass = "http://12.0.0.1:9982"; # Use http here!
+    proxyPass = "http://127.0.0.1:9982"; # Use http here!
     proxyWebsockets = true;
     extraConfig = ''
       proxy_set_header Host $host;
