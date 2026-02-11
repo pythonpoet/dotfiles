bf6a525da827102fa04385e67595473520baa5d9
Your Name <you@example.com>
2026-02-11 12:52:04 +0100

    activating vaultwarden

--- modules/vaultwarden.nix
+++ modules/vaultwarden.nix
@@ -36,7 +36,7 @@ in {
       configureNginx = true;
       
       domain = cfg.domain;
-      backupDir = cfg.data_dir;
+      
 
       #dataDir = lib.mkForce cfg.data_dir;
       # in order to avoid having  ADMIN_TOKEN in the nix store it can be also set with the help of an environment file
@@ -44,17 +44,19 @@ in {
       environmentFile = config.age.secrets.vaultwarden.path;
       config = {
           # Refer to https://github.com/dani-garcia/vaultwarden/blob/main/.env.template
-          SIGNUPS_ALLOWED = false;
+          SIGNUPS_ALLOWED = "false";
 
           ROCKET_ADDRESS = "127.0.0.1";
           ROCKET_PORT = cfg.port;
           ROCKET_LOG = "critical";
 
+          DATA_FOLDER = cfg.data_dir;
+
           SSO_ENABLED="true";
           SSO_AUTHORITY="https://auth.davidwild.ch/application/o/vaultwarden/";
 
-          SSO_SCOPES="openid email profile offline_access";
-          SSO_ALLOW_UNKNOWN_EMAIL_VERIFICATION="true";
+          SSO_SCOPES="email profile offline_access";
+          SSO_ALLOW_UNKNOWN_EMAIL_VERIFICATION="false";
           SSO_CLIENT_CACHE_EXPIRATION="0";
           SSO_ONLY="false"; # Set to true to disable email+master password login and require SSO
           SSO_SIGNUPS_MATCH_EMAIL="true"; # Match fi
