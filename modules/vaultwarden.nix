afc1a59dd429009e6e547e2e38e795b20cb22ece
Your Name <you@example.com>
2026-02-11 15:51:24 +0100

    add datadir attribute to vaultwarden

--- modules/vaultwarden.nix
+++ modules/vaultwarden.nix
@@ -55,7 +55,7 @@ in {
           SSO_ENABLED="true";
           SSO_AUTHORITY="https://auth.davidwild.ch/application/o/vaultwarden/";
 
-          SSO_SCOPES="email profile offline_access";
+          #SSO_SCOPES="email profile offline_access";
           SSO_ALLOW_UNKNOWN_EMAIL_VERIFICATION="false";
           SSO_CLIENT_CACHE_EXPIRATION="0";
           SSO_ONLY="false"; # Set to true to disable email+master password login and require SSO
