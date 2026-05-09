1a0d4909d5d0b56c1294ee439340d149eeb9a96c
Your Name <you@example.com>
2026-05-09 16:31:07 +0200

    add garage

--- modules/s3.nix
+++ modules/s3.nix
@@ -9,7 +9,7 @@
             rpc_bind_addr = "[::]:3901";
             # Generate with: openssl rand -hex 32
             # and store in a secret file, then reference it here
-            rpc_secret_file = "/run/secrets/garage-rpc-secret";
+            rpc_secret_file = "/run/keys/garage-rpc-secret";
 
             s3_api = {
                 s3_region = "zurich";
